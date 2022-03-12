0e169469f8c0defa8a25a95887e8b5d8c97d06d3
wangdandan <962832130@qq.com>
2022-03-12 21:54:25 +0800

    封装request方法，发送请求联调

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from "./router";
+import request from "./utils/request";
 
+Vue.prototype.request = request; 
 Vue.config.productionTip = false;
 new Vue({
   router, //把路由系统注入vue
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  devServer: {
+    proxy: { //代理
+      '/api': { //如果路径是/api开头的,就代理到下面的服务器
+        target: 'http://8.140.36.65:5006',
+        ws: true,
+        changeOrigin: true
+      },
+    }
+  }
+};
