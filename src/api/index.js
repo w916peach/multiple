5c06c73afbff4835111ba2144204b25664f16731
taotao <8073365+cuidctt@user.noreply.gitee.com>
2022-03-13 22:31:49 +0800

    修改线上接口地址

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const host = process.env.NODE_ENV === 'development' ? '' : 'http://8.140.36.65:5006'; // 如果是线上的话，用http://8.140.36.65:5006/  如果是本地的话就用 空字符串
+const host = process.env.NODE_ENV === 'development' ? '' : 'https://8.140.36.65:5008'; // 如果是线上的话，用http://8.140.36.65:5006/  如果是本地的话就用 空字符串
 const API = {
   loginApi: "/api/user/login",
   moodApi: '/api/mood',
