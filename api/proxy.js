70e3bc69c7d49e072e97ee59962639ce6191000c
taotao <8073365+cuidctt@user.noreply.gitee.com>
2022-04-03 00:44:05 +0800

    debug

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
   delete req.query.target;
   const host = 'https://8.140.36.65:5008';
   console.log(host + target, req.method, req.body);
-  const result = await axios(host + target, {method: req.method, data: req.body, params: req.query, headers: req.headers});
+  const result = await axios(host + target, { method: req.method, data: req.body || {}, params: req.query, headers: req.headers});
   console.log(result);
   res.status(result.status);
   res.json(result.data);
