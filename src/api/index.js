7171122a8af1faa6e9dfe903747e90c843b4c4d6
taotao <8073365+cuidctt@user.noreply.gitee.com>
2022-04-03 00:26:29 +0800

    fixed

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
-const host = process.env.NODE_ENV === 'development' ? '' : 'https://8.140.36.65:5008'; // 如果是线上的话，用http://8.140.36.65:5006/  如果是本地的话就用 空字符串
-// const host = '';
+// const host = process.env.NODE_ENV === 'development' ? '' : 'https://8.140.36.65:5008'; // 如果是线上的话，用http://8.140.36.65:5006/  如果是本地的话就用 空字符串
+const host = '';
 const API = {
   loginApi: "/api/user/login",
   moodApi: '/api/mood',
