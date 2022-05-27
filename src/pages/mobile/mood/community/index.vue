c45e8652102fc66a0c3aebf5798f3b6e35862226
taotao <8073365+cuidctt@user.noreply.gitee.com>
2022-05-27 21:58:10 +0800

    修复部分样式，并动态加载了移动端首页数据

--- src/pages/mobile/mood/community/index.vue
+++ src/pages/mobile/mood/community/index.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="community">
     <div class="title">
-      <div class="center">
-        <span @click="$router.push({ name: 'like' })">喜欢</span>
-        <span @click="$router.push({ name: 'friends' })">好友</span>
-        <img src="../../../../assets/images/@interval.png" alt="" />
-      </div>
+      <span @click="$router.push({ name: 'like' })">喜欢</span>
+      <span class="line"></span>
+      <span @click="$router.push({ name: 'friends' })">好友</span>
       <img
+        class="search"
         src="../../../../assets/images/@magnifier.png"
         alt=""
         @click="$router.push({ name: 'search' })"
       />
     </div>
-    <router-view />
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
 >
@@ -30,41 +31,36 @@ export default {
 
 <style scoped>
 .community {
-  height: 100%;
 }
 
 .title {
-  position: relative;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
   height: 0.8rem;
-  /* padding: 0.25rem 0rem; */
   background-color: rgb(230, 107, 107);
   box-shadow: 0rem 0.06rem 0.06rem 0rem rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
-.title > div {
-  width: 2rem;
-  height: 100%;
-  line-height: 0.8rem;
-  margin: 0 auto;
-  text-align: center;
+.title span {
   color: #fff;
-  position: relative;
 }
-
-.title > div > span {
-  margin: 0rem 0.2rem;
+.line {
+  background-color: #fff;
+  width: 2px;
+  height: 25px;
+  margin: 0 20px;
 }
-
-.title > div > img {
+.search {
   position: absolute;
-  top: 0.15rem;
-  left: 1rem;
-}
-
-.title > img {
-  position: absolute;
+  right: 20px;
   top: 50%;
   transform: translateY(-50%);
-  left: 90%;
+}
+.content {
+  padding-top: 0.8rem;
 }
 </style>
