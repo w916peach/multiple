7112856c60c4bb656664c52eea5e03c806b8e77a
王丹丹 <7952261+dandan-wang@user.noreply.gitee.com>
2022-05-27 12:45:03 +0000

    !16 完善登录页布局样式 * 完善登录页样式布局 * 完善引导页尺寸布局 * Merge branch 'develop' of https://gitee.com/babyxkl/multiple into feature/I54CWW * 'fix' * 'fix' * '解决冲突' * 实现详情页路由配置 * 完成酱社区喜欢页、好友页以及搜素页 * 主页配置路由 * 解决冲突 * 完善引导页

--- src/pages/mobile/mood/community/comment.vue
+++ src/pages/mobile/mood/community/comment.vue
@@ -3,73 +3,69 @@
     <div class="main">
       <div class="head">
         <div class="back" @click="$router.push({ name: 'like' })">
-          <img src="../../../../assets/images/@arrow-back.png" alt="">
+          <img src="../../../../assets/images/@arrow-back.png" alt="" />
         </div>
-        <div class="middle">
-          详情
-        </div>
+        <div class="middle">详情</div>
         <div class="more">
-          <img src="../../../../assets/images/@more.png" alt="">
+          <img src="../../../../assets/images/@more.png" alt="" />
         </div>
       </div>
       <div class="content">
-        <div class="title">
-          我喜欢的电影台词
-        </div>
+        <div class="title">我喜欢的电影台词</div>
         <div class="info">
           <div class="face">
-            <img src="../../../../assets/images/@head3.png" alt="">
+            <img src="../../../../assets/images/@head3.png" alt="" />
           </div>
           <div class="user-name">
             <div>
-              <img src="../../../../assets/images/@userlevel.png" alt="">
+              <img src="../../../../assets/images/@userlevel.png" alt="" />
             </div>
-            <div class="level">
-              LV6
-            </div>
+            <div class="level">LV6</div>
           </div>
-          <div class="name">
-            糙汉子啊
-          </div>
+          <div class="name">糙汉子啊</div>
         </div>
         <div class="movie">
-          <img src="../../../../assets/images/@pic3.png" alt="">
+          <img src="../../../../assets/images/@pic3.png" alt="" />
         </div>
         <div class="tip">
           他是拥有山川河海的神明
-          <br>
+          <br />
           她是误入神明眼睛的少女
         </div>
         <div class="likes">
           <div class="ico">
-            <img src="../../../../assets/images/@heart-big.png" alt="">
+            <img src="../../../../assets/images/@heart-big.png" alt="" />
           </div>
           <ul>
-            <li>
-
-            </li>
+            <li></li>
           </ul>
           <div class="arrow-more">
-            <img src="../../../../assets/images/@arrow-r.png" alt="">
+            <img src="../../../../assets/images/@arrow-r.png" alt="" />
           </div>
         </div>
       </div>
     </div>
     <div class="bottom">
       <div class="leavemsg">
-        <input type="text" placeholder="点此评论">
+        <input type="text" placeholder="点此评论" />
       </div>
       <div class="like-wrap">
         <div class="share">
-          <img src="../../../../assets/images/@share.png" alt="">
+          <img src="../../../../assets/images/@share.png" alt="" />
         </div>
         <div class="like">
-          <img src="../../../../assets/images/@heart.png" alt="">
+          <img src="../../../../assets/images/@heart.png" alt="" />
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "comment-page",
+};
+</script>
 
 <style scoped>
 .comment {
@@ -91,8 +87,8 @@
   box-shadow: 0rem 0.02rem 0.2rem 0rem #ccc;
 }
 
-.head>.back,
-.head>.more {
+.head > .back,
+.head > .more {
   width: 0.5rem;
   text-align: center;
 }
