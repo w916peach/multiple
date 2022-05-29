8c8bb4834b6c9310ff3ec9c0ada6816abbf0b2ad
taotao <8073365+cuidctt@user.noreply.gitee.com>
2022-05-29 18:58:46 +0800

    fixed

--- src/pages/AuthLogin/index.vue
+++ src/pages/AuthLogin/index.vue
@@ -42,7 +42,7 @@ export default defineComponent({
     const actionUrl = ref<string>(
       process.env.NODE_ENV === "development"
         ? `http://localhost:5006/api/login/form?appid=${appid}&redirectUrl=${redirectUrl}`
-        : `http://8.140.36.65:5006/api/login/form?appid=${appid}&redirectUrl=${redirectUrl}`
+        : `https://8.140.36.65:5008/api/login/form?appid=${appid}&redirectUrl=${redirectUrl}`
 
       // `/api/proxy?appid=${appid}&redirectUrl=${redirectUrl}&target=/api/login/form`
     );
